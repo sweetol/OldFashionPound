adf4c58c959f2d6103cd73782a4403efeceed517
Vito <Vito@LAPTOP-5EBN8CEK.home-life.hub>
2020-05-16 13:34:46 +0200

    classe abstract per la conversione di tutti i tagli in pence

---
+++ euris-test/src/main/java/euris/test/oldfashionpound/AbstractPenceConverter.java
@@ -0,0 +1,10 @@
+package euris.test.oldfashionpound;
+
+public abstract class AbstractPenceConverter{
+	final Integer getPence() {
+		return getAmount()*getPenceScale();
+	}
+	
+	abstract Integer getAmount();
+	abstract Integer getPenceScale();
+}
